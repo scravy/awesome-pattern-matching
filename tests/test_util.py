2679ff73eb05365767edde3d721188c5ba97ab46
Julian Fleischer <tirednesscankill@warhog.net>
2021-01-05 16:03:54 +0100

    Add test for get_return_type

--- tests/test_util.py
+++ tests/test_util.py
@@ -7,15 +7,19 @@ import apm._util as util
 from apm import *
 
 
+# noinspection PyUnusedLocal
+def f(a: int, b: float, c: str, d: str) -> str:
+    pass
+
 class TypingUtil(unittest.TestCase):
 
     def test_get_arg_types(self):
-        # noinspection PyUnusedLocal
-        def f(a: int, b: float, c: str, d: str) -> str:
-            pass
-
         arg_types = util.get_arg_types(f)
         self.assertTrue(match(
             arg_types,
             [int, float, Remaining(str)]
         ))
+
+    def test_get_return_type(self):
+        self.assertEqual(str, util.get_return_type(f))
+
