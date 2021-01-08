e61ff4565e3c86074ce666499dccf9cbfec842af
Julian Fleischer <tirednesscankill@warhog.net>
2021-01-08 21:33:46 +0100

    Use rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+import sphinx_rtd_theme
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -14,6 +16,11 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+extensions = [
+    "sphinx_rtd_theme",
+]
+
+html_theme = "sphinx_rtd_theme"
 
 # -- Project information -----------------------------------------------------
 
@@ -47,7 +54,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
