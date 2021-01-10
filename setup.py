015d04088817209f9741f931f2bdb6efb8306e92
Julian Fleischer <tirednesscankill@warhog.net>
2021-01-10 23:23:08 +0100

    Publish README to pypi without github toc

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
+import re
+
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+    start_result = re.search(r"<!-- START doctoc [^>]+-->", long_description)
+    if start_result:
+        pos = start_result.span(0)[0]
+        before = long_description[:pos]
+        remaining = long_description[pos:]
+        end_result = re.search(r"<!-- END doctoc [^>]+-->", remaining)
+        end_pos = end_result.span(0)[1]
+        after = remaining[end_pos:]
+        long_description = before + after
 
 __pkginfo__ = {}
 with open("apm/__pkginfo__.py") as fh:
