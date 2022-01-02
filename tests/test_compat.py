8601dec282aba63cdec034c7284cfef9324248b7
Julian Fleischer <tirednesscankill@warhog.net>
2022-01-02 05:29:31 +0100

    Add testcase for compatibility arguments

---
+++ tests/test_compat.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import unittest
+
+from apm import *
+from apm.core import SomePatternCompatibilityArgumentsError
+
+
+class Compat(unittest.TestCase):
+    def test_some_compat(self):
+        self.assertTrue(match([1, 2], [Some(Between(1, 2))]))
+        self.assertTrue(match([1, 2], [Some(pattern=Between(1, 2))]))
+
+    def test_some_compat_conflicting_args(self):
+        with self.assertRaises(SomePatternCompatibilityArgumentsError):
+            Some(1, 2, pattern=3)
+
+    def test_some_compat_no_patterns(self):
+        with self.assertRaises(ValueError):
+            Some()
